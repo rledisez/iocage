02260164aed08c7c11f21b4b1c11cd3d831e28f2
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-10-23 00:21:41 +0500

    Introduce locks for iocage cache
    This commit introduces locks for iocage cache which ensure that when iocage's api is being used, it's safe from cache.reset and cache.datasets/pools simultaneous calls

--- iocage_lib/cache.py
+++ iocage_lib/cache.py
@@ -1,24 +1,32 @@
 from iocage_lib.zfs import all_properties
 
+import threading
+
 
 class Cache:
+
+    cache_lock = threading.Lock()
+
     def __init__(self):
         self.dataset_data = self.pool_data = None
 
     @property
     def datasets(self):
-        if not self.dataset_data:
-            self.dataset_data = all_properties()
-        return self.dataset_data
+        with self.cache_lock:
+            if not self.dataset_data:
+                self.dataset_data = all_properties()
+            return self.dataset_data
 
     @property
     def pools(self):
-        if not self.pool_data:
-            self.pool_data = all_properties(resource_type='zpool')
-        return self.pool_data
+        with self.cache_lock:
+            if not self.pool_data:
+                self.pool_data = all_properties(resource_type='zpool')
+            return self.pool_data
 
     def reset(self):
-        self.dataset_data = self.pool_data = None
+        with self.cache_lock:
+            self.dataset_data = self.pool_data = None
 
 
 cache = Cache()
