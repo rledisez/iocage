e7defc5ae452131627a75a38d81063a7542d3df5
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-03-06 19:01:14 +0500

    Allow passing env when executing script
    (cherry picked from commit 2182f381699c44868c15b630f8945e3bee60ba6a)

--- iocage_lib/ioc_common.py
+++ iocage_lib/ioc_common.py
@@ -820,7 +820,7 @@ def generate_devfs_ruleset(conf, paths=None, includes=None, callback=None,
     return ruleset
 
 
-def runscript(script):
+def runscript(script, custom_env=None):
     """
     Runs the script provided and return a tuple with first value showing
     stdout and last showing stderr
@@ -838,7 +838,7 @@ def runscript(script):
 
     try:
         output = iocage_lib.ioc_exec.SilentExec(
-            script, None, unjailed=True, decode=True
+            script, None, unjailed=True, decode=True, su_env=custom_env
         )
     except iocage_lib.ioc_exceptions.CommandFailed as e:
         return None, f'Script returned non-zero status: {e}'
